7c912114332f1db8d9714a56d605d772f00d8b6d
Igor Babuschkin <igor@babuschk.in>
2013-09-26 13:16:55 +0200

    Beautify survey results

--- Slides/figures/survey.py
+++ Slides/figures/survey.py
@@ -7,10 +7,11 @@ labels = 'Nie', 'Andere Sprache(n)', 'Python'
 plt.pie(
     fracs,
     autopct='%1.1f%%', 
-    colors=('#649600', '#966400', '#006496'),
+    colors=('#729fcf', '#8ae234', '#e9b96e'),
     labels=labels,
     startangle=90
 )
+plt.axis('equal')
 plt.savefig('python_knowledge.pdf', bbox_inches='tight')
 plt.clf()
 
@@ -26,24 +27,29 @@ langs = list(reversed(sorted(set(langlist))))
 counts = [langlist.count(x) for x in langs]
 width = .75
 indices = np.arange(len(langs))
-plt.barh(indices, counts, width, color='#649600')
+plt.barh(indices, counts, width, color='#729fcf')
 plt.yticks(indices + width / 2, langs)
+plt.ylim(-0.75, len(langs)+0.5)
 plt.savefig('otherlangs.pdf', bbox_inches='tight')
 plt.clf()
 
 #  interests
-labels = (
-    'Kommandozeile',
-    'Auswerten mit Python',
-    'Matplotlib',
-    'Autom. Fehlerrechnung',
-    'Automatisierung (make)',
-    'Versionskontrolle'
-)
-counts = (16, 15, 26, 27, 25, 13)
+labels = {
+        'Kommandozeile' : 16,
+        'Auswerten mit Python': 15,
+        'Matplotlib': 26,
+        'Autom. Fehlerrechnung': 27,
+        'Automatisierung (make)': 25,
+        'Versionskontrolle': 13
+}
+
+vals = sorted(labels.values())
+keys = sorted(labels, key=lambda x : labels[x])
 indices = np.arange(len(labels))
-plt.barh(indices, counts, width, color='#649600')
-plt.yticks(indices + width / 2, labels)
+
+plt.barh(indices, vals, width, color='#729fcf')
+plt.yticks(indices + width / 2, keys)
+plt.ylim(-0.50, len(labels)+0.25)
 plt.savefig('interests.pdf', bbox_inches='tight')
 plt.clf()
 
@@ -53,9 +59,10 @@ fracs = 62, 38
 plt.pie(
     fracs,
     autopct='%1.1f%%', 
-    colors=('#649600', '#966400'),
+    colors=('#729fcf', '#8ae234'),
     labels=labels,
     startangle=90
 )
+plt.axis('equal')
 plt.savefig('linux_or_vm.pdf', bbox_inches='tight')
 plt.clf()
