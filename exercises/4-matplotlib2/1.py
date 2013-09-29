e9716ed5a26b0dfd32b747ce2b16d07cf42a043c
Kevin Dungs <kevin@kads24.eu>
2013-09-29 13:18:35 +0200

    Everything in one plot now.

--- exercises/4-matplotlib2/1.py
+++ exercises/4-matplotlib2/1.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 
 x = np.linspace(0, 1, 100)
 for n in range(1, 11):
-    plt.plot(x, x**n)
-    plt.title('$x^{{{}}}$'.format(n))
-    plt.savefig('x_{}.pdf'.format(n))
-    plt.clf()
+    plt.plot(x, x**n, label='$x^{{{}}}$'.format(n))
+plt.legend(loc='upper left')
+plt.savefig('xn.pdf')
