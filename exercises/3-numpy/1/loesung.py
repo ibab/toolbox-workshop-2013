7c6f2f00316bac6a7f5fa48c1073427c408482bd
Ismo Toijala <ismo.toijala@gmail.com>
2013-09-30 21:03:55 +0200

    numpy exercises

--- exercises/3-numpy/1/loesung.py
+++ exercises/3-numpy/1/loesung.py
@@ -5,19 +5,19 @@ from __future__ import (print_function,
                         absolute_import)
 
 import numpy as np
-from numpy.random import rand, randn
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
 # Datenfile erzeugen
+from numpy.random import rand, randn
 N = 50
 data_x = np.linspace(0, 2 * np.pi, N)
 data_y = np.sin(data_x)
 error_y = 0.2 * rand(N) + 0.01
 data_y += error_y * randn(N)
-np.savetxt('1.txt', np.array([data_x, data_y, error_y]).T, header='x y y_err')
+np.savetxt('daten.txt', np.array([data_x, data_y, error_y]).T, header='x y y_err')
 
-x, y, e_y = np.loadtxt('1.txt', unpack=True)
+x, y, e_y = np.loadtxt('daten.txt', unpack=True)
 
 def f(x, a, b, c, d):
     return a * np.sin(b * x + c) + d
@@ -33,4 +33,4 @@ plt.xlim(t[0], t[-1])
 plt.xlabel(r'$t$')
 plt.ylabel(r'$f(t)$')
 plt.legend(loc='best')
-plt.savefig('1.pdf')
+plt.savefig('loesung.pdf')
